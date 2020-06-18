0e293426e2e313828cccb8a3d372c3c8d2bbd097
Mudit <muditw@amazon.com>
2020-06-19 00:44:13 +0530

    :)

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const rateLimit = require("express-rate-limit");
+let data = require('./data/json data');
 
 const requestHashKey = (req) => {
     return req.headers['x-forwarded-for'];
@@ -30,6 +31,31 @@ async function sleep(ms) {
 router.post('/bPMAVm', limiter, async function (req, res, next) {
     // process req.body => {fullName, dateOfBirth}
     res.json("result");
+});
+
+router.get('/giveMeData', function (req, res) {
+    res.json(data);
+});
+
+router.get('/levelUno', function (req, res) {
+    res.json(Object.keys(data));
+});
+
+router.get('/levelDuos/:parent', function (req, res) {
+    res.json(Object.keys(data[req.params.parent]));
+});
+
+router.get('/levelTres/:grandparent/:parent', function (req, res) {
+    res.json(Object.keys(data[req.params.grandparent][req.params.parent]));
+});
+
+router.get('/levelQuatro/:greatgrandparent/:grandparent/:parent', function (req, res) {
+    let result = data[req.params.greatgrandparent][req.params.grandparent][req.params.parent];
+    if(typeof result === "string") {
+        res.json([]);
+        return;
+    }
+    res.json(Object.keys(result));
 });
 
 module.exports = router;
